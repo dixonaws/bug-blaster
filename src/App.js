b5821b2eb7e50d08ee5fb607ab00d3bd894c940f
John Dixon <jpdixon@gmail.com>
2024-10-08 23:01:33 +0200

    video 69 - added ticket list

--- src/App.js
+++ src/App.js
@@ -3,20 +3,31 @@ import './styles.css';
 import TicketForm from "./components/TicketForm";
 import ticketReducer from "./reducers/ticketReducer";
 import {useReducer} from "react";
+import TicketList from "./components/TicketList";
 
 function App() {
     const initialState = {
         tickets: []
     }
 
-    const [state, dispatch]=useReducer(ticketReducer, initialState);
-
+    const [state, dispatch] = useReducer(ticketReducer, initialState);
 
     return (
         <div className="App">
             <div className='container'>
                 <h1>Bug Blaster</h1>
                 <TicketForm dispatch={dispatch}/>
+                {state.tickets.length > 0 &&
+                    <div className="results">
+                        <h2>All Tickets</h2>
+
+                        <TicketList
+                            tickets={state.tickets}
+                            dispatch={dispatch}/>
+                    </div>
+                }
+
+
             </div>
         </div>
     );
