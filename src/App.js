a344dcd15fd1f6c705692e01cc183b5ac8b7fcc6
John Dixon <jpdixon@gmail.com>
2024-10-08 17:08:39 +0200

    video 62

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import './styles.css';
+import TicketForm from "./components/TicketForm";
 
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
         <div className="App">
             <div className='container'>
                 <h1>Bug Blaster</h1>
+                <TicketForm/>
             </div>
         </div>
     );
